dea2f23d2e52c7436b9dea4fa91122aabf395d60
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2025-01-19 15:48:20 +0100

    🗃️ Add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
+import debug from 'debug';
 import express from 'express';
+import mysql from 'mysql';
+import { HOST, USER, PASSWORD, DATABASE } from './config.js';
 
+const msg = debug('PAT:index');
 const app = express();
-const port = 5000;
+const port = 3000;
+
+const db = mysql.createConnection({
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  database: DATABASE,
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript Node Express!');
+  res.json('Hello, TypeScript Node Express!');
+});
+app.get('/users', (req, res) => {
+  const sql = 'SELECT * FROM users';
+  db.query(sql, (err, data) => {
+    if (err) return res.json('ERROR');
+    else return res.json(data);
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port:  localhost:${port}`);
+  msg(`Listening on ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
