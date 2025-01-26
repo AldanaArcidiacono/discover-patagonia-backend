97cc3197c39376eb1c322be01dc5a2e6ad7a5796
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2025-01-26 15:18:08 +0100

    ⚡️ Update controller+repo to classes & Add endpoints

--- src/router/usersRouter.ts
+++ src/router/usersRouter.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/usersController.js';
+import { UsersController } from '../controllers/usersController.js';
+import { UsersRepo } from '../repositories/usersRepo.js';
 
 export const usersRouter = Router();
 
-// GET /users
-usersRouter.get('/', getAllUsers);
+const usersController = new UsersController(UsersRepo.getInstance());
+
+// READ
+usersRouter.get('/', usersController.getAllUsers.bind(usersController));
+usersRouter.get('/:id', usersController.getUserById.bind(usersController));
+// CREATE
+usersRouter.post('/', usersController.addUser.bind(usersController));
